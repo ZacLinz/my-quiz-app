e2e264d96fd55508f4d136d9ecec4a94e9b826d3
ZacLinz <zaclinzmeyer@gmail.com>
2019-07-07 21:29:18 -0600

    add styling

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -11,7 +11,7 @@ export class WelcomeComponent implements OnInit {
 
   quiz: Quiz[];
 
-  constructor(private questionsService: QuestionsService) {}
+  constructor(public questionsService: QuestionsService) {}
 
   ngOnInit() {
 
